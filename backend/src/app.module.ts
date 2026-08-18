2fd12f00d48cc9cb12ab99fa5a77f7055c7d23ea
Victor Junior da Silva Ramos <victorjr.muchacho@gmail.com>
2026-08-18 18:35:23 -0300

    build: configure Prisma infrastructure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaModule } from './prisma/prisma.module';
 import { SystemModule } from './modules/system/system.module';
 
 @Module({
-  imports: [SystemModule],
+  imports: [PrismaModule, SystemModule],
   controllers: [],
   providers: [],
 })
