24a6708b0ff0972c545c25481277b908ac3fd2d8
Victor Junior da Silva Ramos <victorjr.muchacho@gmail.com>
2026-08-31 15:35:38 -0300

    chore: ajustar lint do backend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'src/generated/prisma/**', '**/*.spec.ts'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
